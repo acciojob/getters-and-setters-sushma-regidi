0f4e2e79b277c5e3497a1a9196d1d8e74cd4f7ee
sushma-regidi <regidisushma@gmail.com>
2025-10-26 16:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-//complete this code
-class Person {}
+// Base class: Person
+class Person {
+  constructor(name, age) {
+    this._name = name;  // using underscore to indicate private-like property
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+// Subclass: Student
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Subclass: Teacher
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// --- Example usage ---
+// Uncomment to test in browser console or Node.js
+
+// const person = new Person("John", 25);
+// console.log(person.name); // Output: John
+// person.age = 30;
+// console.log(person.age);  // Output: 30
+
+// const student = new Student("Alice", 22);
+// student.study();          // Output: Alice is studying
+
+// const teacher = new Teacher("Bob", 40);
+// teacher.teach();          // Output: Bob is teaching
+
